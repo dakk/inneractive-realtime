5b5d437f191508e694734008c20205cd1c5c95ee
Davide Gessa <gessadavide@gmail.com>
2014-02-01 10:53:06 +0100

    removed urls

--- log.php
+++ log.php
@@ -1,9 +1,6 @@
 <?php
 	$f = fopen ('log.txt', 'a');
 
-	//http://sagregna.it/advlog/log.php?type=click&ip=$ip$&timestamp=$timestamp$&networkName=$networkName$&country=$country$&city=$city$&os=$os$&appname=$appname$&age=$age$&model=$model$&brand=$brand$
-
-
 	$string = ($_GET['type']).','.($_GET['networkName']).','.($_GET['ip']).','.($_GET['timestamp']).','.($_GET['country']).','.($_GET['city']).',';
 	$string .= ($_GET['os']).','.($_GET['brand']).','.($_GET['model']).','.($_GET['appname'])."\n";
 
